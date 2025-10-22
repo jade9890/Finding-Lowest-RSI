2fc896a0253617d5cccd934337426e53e3fdb825
jade9890 <jadeluong789@gmail.com>
2025-10-22 12:25:26 +0100

    retrieved the s&p 500 stocks

--- findLowestRSI.py
+++ findLowestRSI.py
@@ -1,3 +1,14 @@
 import pandas as pd
-print(pd.__version__)
+import talib as ta
+import yfinance as yf
 
+#scrape wikipedia table
+url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+tables = pd.read_html(url)
+
+#The first table contains the S&P 500 companies
+sp500 = tables[0]
+
+
+#Extract the symbols (tickers)
+tickers = sp500['Symbol'].tolist()
