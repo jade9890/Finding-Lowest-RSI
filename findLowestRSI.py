7c9596856005a97192438ce4f84c99e745a559f1
jade9890 <jadeluong789@gmail.com>
2025-10-22 12:36:32 +0100

    close price and rsi

--- findLowestRSI.py
+++ findLowestRSI.py
@@ -11,4 +11,19 @@ sp500 = tables[0]
 
 
 #Extract the symbols (tickers)
-tickers = sp500['Symbol'].tolist()
+tickersList = sp500['Symbol'].tolist()
+
+
+#download data for different intervals of time
+oneDayData = yf.download(tickersList, period="max", interval="1d", group_by='ticker')
+fourHourData = yf.download(tickersList, period ="1yr", intervals="4h", group_by='ticker')
+
+
+#get close prices
+closePrice1D = oneDayData.xs("Close", axis=1, level=1)
+closePrice4Hr = fourHourData.xs("Close", axis=1, level=1)
+
+
+#get rsi based on close prices
+rsiOneDay = pd.DataFrame({ticker: ta.RSI(closePrice1D[ticker],timeperiod=14) for ticker in tickersList})
+rsiFourHour = pd.DataFrame({ticker: ta.RSI(closePrice4Hr[ticker],timeperiod=14) for ticker in tickersList})
